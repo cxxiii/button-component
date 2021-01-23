0af31f3a428c0f850ae4e16dfc9ba28a411018b0
Nikolay Rademacher <hrrdx33@gmail.com>
2021-01-23 07:46:09 +0100

    finalize & update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,78 @@
 import React from "react";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <button>Hello World</button>;
+  return (
+    <>
+      <h1 style={{ textAlign: "center", textTransform: "uppercase" }}>
+        Buttons
+      </h1>
+      <br />
+      <div className="display-box_container">
+        <div className="display-box">
+          <pre>{`<Button />`}</pre>
+          <Button>Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button variant=”outline” />`}</pre>
+          <Button variant="outline">Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button variant=”text” />`}</pre>
+          <Button variant="text">Hello World</Button>
+        </div>
+      </div>
+      <div className="display-box_container">
+        <div className="display-box">
+          <pre>{`<Button disableShadow />`}</pre>
+          <Button disableShadow>Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button disabled />`}</pre>
+          <Button disabled>Hello World</Button>
+        </div>
+      </div>
+      <div className="display-box_container">
+        <div className="display-box">
+          <pre>{`<Button startIcon=”local_grocery_store” />`}</pre>
+          <Button startIcon="local_grocery_store">Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button endIcon=”local_grocery_store” />`}</pre>
+          <Button endIcon="local_grocery_store">Hello World</Button>
+        </div>
+      </div>
+      <div className="display-box_container">
+        <div className="display-box">
+          <pre>{`<Button size="sm" />`}</pre>
+          <Button size="sm">Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button size="md" />`}</pre>
+          <Button size="md">Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button size="lg" />`}</pre>
+          <Button size="lg">Hello World</Button>
+        </div>
+      </div>
+      <div className="display-box_container">
+        <div className="display-box">
+          <pre>{`<Button color="primary" />`}</pre>
+          <Button color="primary">Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button color="secondary" />`}</pre>
+          <Button color="secondary">Hello World</Button>
+        </div>
+        <div className="display-box">
+          <pre>{`<Button color="danger" />`}</pre>
+          <Button color="danger">Hello World</Button>
+        </div>
+      </div>
+      <footer>Nikolay Rademacher</footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,49 @@
+import React, { FunctionComponent, ReactNode } from "react";
+import styles from "./Button.module.scss";
+import cn from "classnames";
+
+type Variants = "outline" | "text";
+type Sizes = "sm" | "md" | "lg";
+type Colors = "default" | "primary" | "secondary" | "danger";
+
+const Button: FunctionComponent<
+  {
+    children: String | ReactNode;
+    variant?: Variants;
+    disableShadow?: boolean;
+    disabled?: boolean;
+    startIcon?: String;
+    endIcon?: String;
+    size?: Sizes;
+    color?: Colors;
+  } & React.HTMLAttributes<HTMLButtonElement>
+> = ({
+  children,
+  variant,
+  disableShadow,
+  disabled,
+  startIcon,
+  endIcon,
+  size,
+  color,
+  ...props
+}) => {
+  const classNames = cn({
+    [styles.Button]: Button,
+    [styles[`Button_variant_${variant}`]]: variant,
+    [styles.Button_disableShadow]: disableShadow,
+    [styles.Button_disabled]: disabled,
+    [styles[`Button_size_${size}`]]: size,
+    [styles[`Button_color_${color}`]]: color,
+  });
+
+  return (
+    <button className={classNames} {...props}>
+      {startIcon && <i className="material-icons start-icon">{startIcon}</i>}
+      {children}
+      {endIcon && <i className="material-icons end-icon">{endIcon}</i>}
+    </button>
+  );
+};
+
+export default Button;
